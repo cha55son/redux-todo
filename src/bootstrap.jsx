6d9b79ae9cfa6dc984f766501d5abf0c700b6a6d
Chason Choate <cha55son@gmail.com>
2015-12-13 22:33:21 -0600

    Added redux dev tools.

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -5,6 +5,8 @@ import { Provider, connect } from 'react-redux'
 import { Router, Route, Link, IndexRoute } from 'react-router'
 import { syncReduxAndRouter } from 'redux-simple-router'
 import createHashHistory from 'history/lib/createHashHistory'
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 import reducers from './reducers/app'
 
@@ -18,7 +20,11 @@ import NotFound from './components/NotFound'
 // Delete once we eliminate the fixtures
 import { TODO_CREATE } from './actions/todos'
 
-const store = createStore(reducers)
+const finalCreateStore = compose(
+    devTools(),
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)(createStore)
+const store = finalCreateStore(reducers)
 const history = createHashHistory({ queryKey: false })
 syncReduxAndRouter(history, store)
 
@@ -26,28 +32,40 @@ syncReduxAndRouter(history, store)
 store.dispatch({
     type: TODO_CREATE,
     props: {
-        title: 'Pick Quinn up',
+        title: 'Decorate the christmas tree',
+        completed: true
+    }
+})
+store.dispatch({
+    type: TODO_CREATE,
+    props: {
+        title: 'Buy Christmas presents',
         completed: false
     }
 })
 store.dispatch({
     type: TODO_CREATE,
     props: {
-        title: 'Decorate the christmas tree',
-        completed: true
+        title: 'Open Christmas presents',
+        completed: false
     }
 })
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <Route path="/" component={App}>
-                <IndexRoute component={TodoList} />
-                <Route path="add" component={TodoAddItem} />
-                <Route path="edit/:id" component={TodoEditItem} />
-            </Route>
-            <Route path="*" component={NotFound}/>
-        </Router>
-    </Provider>,
+    <div>
+        <Provider store={store}>
+            <Router history={history}>
+                <Route path="/" component={App}>
+                    <IndexRoute component={TodoList} />
+                    <Route path="add" component={TodoAddItem} />
+                    <Route path="edit/:id" component={TodoEditItem} />
+                </Route>
+                <Route path="*" component={NotFound}/>
+            </Router>
+        </Provider>
+        <DebugPanel top right bottom>
+            <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>
+    </div>,
     document.getElementById('app-container')
 )
--- src/reducers/app.js
+++ src/reducers/app.js
@@ -2,14 +2,8 @@ import { combineReducers } from 'redux'
 import { routeReducer } from 'redux-simple-router'
 
 import { reduceTodos } from './todos'
-import { reduceRoutes } from './routes'
 
 export default combineReducers({
-    everything: function(state = {}, action) {
-        console.log(action)
-        return state
-    },
     routing: routeReducer,
-    routes: reduceRoutes,
     todos: reduceTodos
 })
